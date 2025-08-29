563ffd4290b93ff98c7725f404ac59ae26a2a6b8
okolo kamdilichukwu samuel <150658859+boi-network12@users.noreply.github.com>
2025-08-29 02:51:19 +0100

    refactor: clean up formatting and improve readability in TabsLayout component

--- app/app/(tabs)/_layout.js
+++ app/app/(tabs)/_layout.js
@@ -1,25 +1,27 @@
-// app/(protected)/_layout.js or _layout.tsx
 import { Tabs } from 'expo-router';
 import CustomTabs from '../../components/custom/CustomTabs';
 import { useTheme } from '../../context/ThemeContext';
 import { getThemeColors } from '../../utils/theme';
 
-
 export default function TabsLayout() {
   const { isDark } = useTheme();
   const colors = getThemeColors(isDark);
 
-
   return (
     <Tabs
-       tabBar={(props) => <CustomTabs {...props} colors={colors} />}
-       screenOptions={{
+      tabBar={(props) => <CustomTabs {...props} colors={colors} />}
+      screenOptions={{
         headerShown: false,
-         tabBarStyle: {
-            display: "none"
-         }
-       }}
+        tabBarStyle: {
+          display: 'none',
+        },
+      }}
     >
+      <Tabs.Screen name="home/index" />
+      <Tabs.Screen name="categories/index" />
+      <Tabs.Screen name="post/index" />
+      <Tabs.Screen name="message/index" />
+      <Tabs.Screen name="profile/index" />
     </Tabs>
   );
 }
